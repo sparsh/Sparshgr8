6fb034a7c913fe5a0856e1f51b8fb2ae38181e01
sparsh <sparshgr8@yahoo.com>
2017-04-24 03:38:22 +0530

    Empty Activity Android Studio

---
+++ app/src/main/java/sparshgr8/in/MainActivity.java
@@ -0,0 +1,15 @@
+package sparshgr8.in;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sparshgr8.in.sparshgr8.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
