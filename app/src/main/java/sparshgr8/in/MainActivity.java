329b91a4edf22fa7651d11c48a147c1b776b42c3
sparsh <sparshgr8@yahoo.com>
2017-04-25 00:12:07 +0530

    added font for textview and edit text and added firebase also designed first page

--- app/src/main/java/sparshgr8/in/MainActivity.java
+++ app/src/main/java/sparshgr8/in/MainActivity.java
@@ -1,9 +1,10 @@
 package sparshgr8.in;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import sparshgr8.in.sparshgr8.R;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((EditText)findViewById(R.id.passcode)).addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                if(s!=null)
+                {
+
+                }
+                {
+
+                }
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/sparshgr8/in/views/NewEditText.java
@@ -0,0 +1,118 @@
+package sparshgr8.in.views;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.support.v7.widget.AppCompatEditText;
+import android.util.AttributeSet;
+
+import sparshgr8.in.R;
+
+
+/**
+ * Created by appsquadz on 4/5/16.
+ */
+public class NewEditText extends AppCompatEditText {
+
+    private Typeface typefaceMedium, typefaceRegular, typefaceBold, typefaceLite;
+
+    public NewEditText(Context context) {
+        super(context);
+        loadTypeFace(context);
+        init(null);
+    }
+
+    public NewEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        loadTypeFace(context);
+        init(attrs);
+    }
+
+    public NewEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        loadTypeFace(context);
+        init(attrs);
+    }
+
+    public enum FONT_VAL {
+        REGULAR(2), BOLD(3), LIGHT(0), MEDIUM(1), NONE(4);
+        private final int ID;
+
+        FONT_VAL(final int id) {
+            this.ID = id;
+        }
+
+        public int getId() {
+            return ID;
+        }
+    }
+
+    private void init(AttributeSet attrs) {
+//        if (attrs != null) {
+//            TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.NewEditText);
+//            String fontName = a.getString(R.styleable.NewEditText_font);
+//            if (fontName != null) {
+//                Typeface myTypeface = Typeface.createFromAsset(getContext().getAssets(), "fonts/" + fontName);
+//                setTypeface(myTypeface);
+//            } else {
+//                setTypeface(typefaceRegular);
+//            }
+//            a.recycle();
+//        } else {
+//            setTypeface(typefaceRegular);
+//        }
+
+        if (attrs != null) {
+            TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.FontStyle);
+            FONT_VAL font_val= FONT_VAL.NONE;
+            try {
+                for (FONT_VAL mode : FONT_VAL.values()) {
+                    if (a.getInt(R.styleable.FontStyle_font, 2) == mode.getId()) {
+                        font_val = mode;
+                        break;
+                    }
+                }
+                if (font_val == FONT_VAL.REGULAR) {
+                    setTypeface(typefaceRegular);
+                }else if(font_val== FONT_VAL.BOLD){
+                    setTypeface(typefaceBold);
+                }else if(font_val== FONT_VAL.LIGHT){
+                    setTypeface(typefaceLite);
+                } else if (font_val == FONT_VAL.MEDIUM){
+                    setTypeface(typefaceMedium);
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            a.recycle();
+        } else {
+            setTypeface(typefaceRegular);
+        }
+    }
+
+    public void loadTypeFace(Context mContext) {
+        if (typefaceMedium == null)
+            typefaceMedium = Typeface.createFromAsset(mContext.getAssets(), "fonts/Roboto-Medium.ttf");
+        if (typefaceBold == null)
+            typefaceBold = Typeface.createFromAsset(mContext.getAssets(), "fonts/Roboto-Bold.ttf");
+        if (typefaceRegular == null)
+            typefaceRegular = Typeface.createFromAsset(mContext.getAssets(), "fonts/Roboto-Regular.ttf");
+        if (typefaceLite == null)
+            typefaceLite = Typeface.createFromAsset(mContext.getAssets(), "fonts/Roboto-Light.ttf");
+    }
+//    private void init(AttributeSet attrs) {
+//        if (attrs != null) {
+//            TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.NewEditText);
+//            String fontName = a.getString(R.styleable.NewEditText_font);
+//            if (fontName != null) {
+//                Typeface myTypeface = Typeface.createFromAsset(getContext().getAssets(), "fonts/" + fontName);
+//                setTypeface(myTypeface);
+//            } else {
+//                setTypeface(typefaceRegular);
+//            }
+//            a.recycle();
+//        } else {
+//            setTypeface(typefaceRegular);
+//        }
+//    }
+}
